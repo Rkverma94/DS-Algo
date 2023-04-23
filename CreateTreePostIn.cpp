d8875db51a8aadfbc0c8ec0b82e86863d90fd697
Rakesh Kumar Verma <rakeshverma1994@gmail.com>
2023-04-23 02:23:51 +0000

    Search method taking O(n) -> improved

--- CreateTreePostIn.cpp
+++ CreateTreePostIn.cpp
@@ -18,28 +18,30 @@ class CreateTreePostIn {
         CreateTreePostIn() {
             root = NULL;
         }
-        Node* generateTree(vector<int>, vector<int>, int, int);
-        int search(vector<int>, int, int, int);
+        Node* generateTree(vector<int>, vector<int>, int, int, unordered_map<int, int>);
+        //int search(vector<int>, int, int, int);
         void preOrder(Node*);
 };
 
-int CreateTreePostIn :: search(vector<int> inOrder, int startIdx, int endIdx, int key) {
+/*int CreateTreePostIn :: search(vector<int> inOrder, int startIdx, int endIdx, int key) {
+    //This search method is taking O(n) to search.
+    //We can remove this by changing search to a hashmap or inorders
     for(int i = startIdx; i <= endIdx; i++) {
         if(inOrder[i] == key) return i;
     }
     return -1;
-}
+}*/
 
-Node* CreateTreePostIn :: generateTree(vector<int> inOrder, vector<int> postOrder, int inStart, int inEnd) {
+Node* CreateTreePostIn :: generateTree(vector<int> inOrder, vector<int> postOrder, int inStart, int inEnd, unordered_map<int, int> map) {
     if(inStart > inEnd) return NULL;
     static int postIdx = postOrder.size() - 1;
     Node* newNode = new Node(postOrder[postIdx]);
     postIdx--;
     if(inStart == inEnd) return newNode;
-    int inIdx = search(inOrder, inStart, inEnd, newNode->data);
+    int inIdx = map[newNode->data];//search(inOrder, inStart, inEnd, newNode->data);
     if(inIdx == -1) return newNode;
-    newNode->right = generateTree(inOrder, postOrder, inIdx+1, inEnd);
-    newNode->left = generateTree(inOrder, postOrder, inStart, inIdx - 1);
+    newNode->right = generateTree(inOrder, postOrder, inIdx+1, inEnd, map);
+    newNode->left = generateTree(inOrder, postOrder, inStart, inIdx - 1, map);
     return newNode;
 }
 
@@ -54,7 +56,11 @@ int main() {
     vector<int> inOrder = {56, 12, 78, 10, 122, 14, 34};
     vector<int> postOrder = {56, 78, 12, 122, 34, 14, 10};
     CreateTreePostIn t;
-    t.root = t.generateTree(inOrder, postOrder, 0, inOrder.size() - 1);
+    unordered_map<int, int> map;
+    for(int i=0; i<inOrder.size(); i++) {
+        map[inOrder[i]] = i;
+    }
+    t.root = t.generateTree(inOrder, postOrder, 0, inOrder.size() - 1, map);
     t.preOrder(t.root);
     return 0;
 }
--- CreateTreePreIn.cpp
+++ CreateTreePreIn.cpp
@@ -17,21 +17,23 @@ class CreateTree {
         CreateTree() {
             root = NULL;
         }
-        Node* generateTree(vector<int>, vector<int>, int, int);
-        int search(vector<int>, int, int, int);
+        Node* generateTree(vector<int>, vector<int>, int, int, unordered_map<int, int>);
+        //int search(vector<int>, int, int, int);
         void postOrder(Node*);
 };
 
-int CreateTree :: search(vector<int> inOrder, int startIdx, int endIdx, int data) {
+/*int CreateTree :: search(vector<int> inOrder, int startIdx, int endIdx, int data) {
+    //This search method is taking O(n) to search.
+    //We can remove this by changing search to a hashmap or inorders
     for(int i = startIdx; i <= endIdx; i++) {
         if(inOrder[i] == data) {
             return i;
         }
     }
     return -1;
-}
+}*/
 
-Node* CreateTree :: generateTree(vector<int> preOrder, vector<int> inOrder, int inStart, int inEnd) {
+Node* CreateTree :: generateTree(vector<int> preOrder, vector<int> inOrder, int inStart, int inEnd, unordered_map<int, int> map) {
     cout<<"---Start----"<<endl;
     static int preIdx = 0;
     //if indices are overlapping or crossing then return null
@@ -41,11 +43,11 @@ Node* CreateTree :: generateTree(vector<int> preOrder, vector<int> inOrder, int 
     //if this is leaf node then return it
     if(inStart == inEnd) return newNode;
 
-    int inIdx = search(inOrder, inStart, inEnd, newNode->data);
+    int inIdx = map[newNode->data];//search(inOrder, inStart, inEnd, newNode->data);
     cout<<"InOrder Idx found :: "<<inIdx<<endl;
     if(inIdx == -1) return newNode;
-    newNode->left = generateTree(preOrder, inOrder, inStart, inIdx - 1);
-    newNode->right = generateTree(preOrder, inOrder, inIdx + 1, inEnd);
+    newNode->left = generateTree(preOrder, inOrder, inStart, inIdx - 1, map);
+    newNode->right = generateTree(preOrder, inOrder, inIdx + 1, inEnd, map);
     cout<<"------end------"<<endl;
     return newNode;
 }
@@ -61,7 +63,11 @@ int main() {
     vector<int> inOrder = {56, 12, 78, 10, 122, 14, 34};
     vector<int> preOrder = {10, 12, 56, 78, 14, 122, 34};
     CreateTree t;
-    t.root = t.generateTree(preOrder, inOrder, 0, inOrder.size() - 1);
+    unordered_map<int, int> map;
+    for(int i=0; i<inOrder.size(); i++) {
+        map[inOrder[i]] = i;
+    } 
+    t.root = t.generateTree(preOrder, inOrder, 0, inOrder.size() - 1, map);
     t.postOrder(t.root);
     return 0;
 }
